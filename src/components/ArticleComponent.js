53aeb24a70848d21f451b631123e545a844cd4b9
vishwa <karkavishwanath230@gmail.com>
2022-11-08 08:29:59 +0530

    components divided

---
+++ src/components/ArticleComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ArticleComponent() {
+  return (
+    <div>ArticleComponent</div>
+  )
+}
+
+export default ArticleComponent
---
+++ src/components/ArticleSkeleton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ArticleSkeleton() {
+  return ( 
+    <div>ArticleSkeleton</div>
+  )
+}
+
+export default ArticleSkeleton  
